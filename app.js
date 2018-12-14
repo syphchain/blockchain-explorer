8d4ada64a04fdf496e66ef98fd05e80764404a4a
siger <Sun597@163.com>
2018-12-14 23:15:17 +0800

    增加swagger和log管理

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const Koa = require('koa')
+const fs = require('fs')
 const app = new Koa()
 const router = require('koa-router')()
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-
+// 默认的koa-logger默认设置的拦截打印请求
+const koalogger = require('koa-logger')
+// log4js配置
+const log4js = require('log4js')
+const log4js_config = require('./config/log4js.json')
+log4js.configure(log4js_config)
+// 路由处理相关
 const url_filter = require('./middlewares/response_formatter')
 const index = require('./routes/index')
 const users = require('./routes/users')
 const api = require('./routes/api')
+
+// swagger api文档相关
+const port = process.env.port || 3000
+// swagger-ui配置获取文档并部署到ip:prot/docs上
+const koaSwagger = require('koa2-swagger-ui')
+app.use(koaSwagger({
+  routePrefix: '/docs',
+  swaggerOptions: {
+    url: `http://localhost:${port}/swagger`
+  }
+}))
 
 // error handler
 onerror(app)
@@ -20,7 +37,7 @@ app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
-app.use(logger())
+app.use(koalogger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
 const router = require('koa-router')()
+// 获取api文档
+const swagger = require('../common/swaggerDoc.js')
+router.get('/swagger', async (ctx, next) => {
+  ctx.body = swagger()
+})
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Returns the homepage
+ *     responses:
+ *       200:
+ *         description: hello world
+ */
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
